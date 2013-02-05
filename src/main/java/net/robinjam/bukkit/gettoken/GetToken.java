b41913467d4f45b17d7ba3f9a36a479b91f073aa
James Robinson <admin@robinjam.net>
2013-02-05 02:12:12 +0000

    Change user tokens to 7 digits of base 62

--- src/main/java/net/robinjam/bukkit/gettoken/GetToken.java
+++ src/main/java/net/robinjam/bukkit/gettoken/GetToken.java
@@ -51,7 +51,26 @@ public class GetToken extends JavaPlugin {
 	public static String getToken(String username, String salt) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("SHA-512");
 		byte[] digest = md.digest((username + salt).getBytes());
-		return String.format("%0128x", new BigInteger(1, digest)).substring(0, 5);
+		return base62_encode(new BigInteger(1, digest)).substring(0, 7);
+	}
+	
+	/**
+	 * Base 62 encodes the given integer.
+	 * 
+	 * @param num The number to encode. Negative numbers are supported.
+	 * @return The encoded number.
+	 */
+	public static String base62_encode(BigInteger num) {
+		char[] primitives = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+		BigInteger base = BigInteger.valueOf(primitives.length);
+		boolean neg = num.compareTo(BigInteger.ZERO) < 0;
+		num = num.abs();
+		String result = (num.compareTo(BigInteger.ZERO) == 0) ? "0" : "";
+		while (num.compareTo(BigInteger.ZERO) > 0) {
+			result = primitives[num.divideAndRemainder(base)[1].intValue()] + result;
+			num = num.divide(base);
+		}
+		return neg ? "-" + result : result;
 	}
 	
 }
--- src/test/java/net/robinjam/bukkit/gettoken/GetTokenTest.java
+++ src/test/java/net/robinjam/bukkit/gettoken/GetTokenTest.java
@@ -10,7 +10,9 @@ public class GetTokenTest {
 	
 	@Test
 	public void testGetToken() throws NoSuchAlgorithmException {
-		assertEquals("7d73c", GetToken.getToken("robinjam", "NaCl"));
+		String salt = "60605fba88917c65aa6a848c525acbd48f1cd4526f6c52f0bc135e5a3383683aed80f83767a84e01ebec11772a1dfd7fc6cf6f1b400346363969e66acfc75a41";
+		assertEquals("W2Nj4za", GetToken.getToken("foo", salt));
+		assertEquals("61I7FP6", GetToken.getToken("bar", salt));
 	}
 	
 }
